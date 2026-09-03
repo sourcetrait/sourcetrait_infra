04e51f5a847c924e9078abfc5984e134959cc8b9
roylaurie <roy.laurie.pub@sourcetrait.com>
2026-09-03 14:28:55 +0000

    wip lab

--- lab/ad/member/unattend/unattend_pwsh.ps1
+++ lab/ad/member/unattend/unattend_pwsh.ps1
@@ -2,9 +2,5 @@
 $ErrorActionPreference = 'Stop'
 Set-PSDebug -Trace 1
 
-Start-Transcript -Path "C:\Windows\Temp\unattend_pwsh.log"
-
 # install latest powershell
 Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet"
-
-Stop-Transcript
