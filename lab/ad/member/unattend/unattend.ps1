6d96b8c6019fa1b0a8b631d3ffc7788de22ac19d
roylaurie <roy.laurie.pub@sourcetrait.com>
2026-09-04 00:59:50 +0000

    wip

--- lab/ad/member/unattend/unattend.ps1
+++ lab/ad/member/unattend/unattend.ps1
@@ -105,8 +105,8 @@ function step_user_lab {
     $ssh = 'C:\Users\lab\.ssh'
     Copy-Item 'E:\.ssh' $ssh -Recurse
     Get-ChildItem $ssh -Recurse -File | ForEach-Object { $_.IsReadOnly = ($_.Name -ne 'authorized_keys') }
-    icacls.exe $ssh /setowner lab
-    icacls.exe $ssh /inheritance:r /grant 'lab:(OI)(CI)F' /grant 'SYSTEM:(OI)(CI)F' /t /c
+    icacls.exe $ssh /setowner lab /t /c
+    icacls.exe $ssh /inheritance:r /grant 'lab:(OI)(CI)F' /grant 'SYSTEM:(OI)(CI)F'
 }
 
 function step_defender {
